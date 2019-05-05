72fcca5f56b2f53a089531dbef1f8bad8058bbae
Vishal R <vishal27.rs@outlook.com>
2019-05-05 12:25:32 +0530

    Added one_hot_encoder test

--- bandits/tests/test_data.py
+++ bandits/tests/test_data.py
@@ -2,14 +2,13 @@ import urllib.request as request
 import urllib
 import os
 import sys
+import pandas as pd
 from os.path import dirname as d
 from os.path import abspath, join
 root_dir = d(d(d(abspath(__file__))))
 sys.path.append(root_dir)
 
-print(sys.path)
-
-from bandits.data.data_preprocessor import download_data
+from bandits.data.data_preprocessor import download_data, one_hot_encoder
 
 def test_download_data():
     file_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data"
@@ -25,3 +24,16 @@ def test_download_data():
     except:
         pass
     assert site.length == file_size
+
+def test_one_hot_encoder():
+    test_df = pd.DataFrame()
+    test_df['col1'] = list(range(3))*10
+    encoded_df = pd.DataFrame()
+    encoded_df['col1 0'] = [1,0,0]*10
+    encoded_df['col1 1'] = [0,1,0]*10
+    encoded_df['col1 2'] = [0,0,1]*10
+    module_encoded_df = one_hot_encoder(test_df, ['col1'])
+    for i in range(len(module_encoded_df.columns)):
+        module_encoded_col = module_encoded_df.columns[i]
+        encoded_col = encoded_df.columns[i]
+        assert module_encoded_df[module_encoded_col].astype(type(encoded_df[encoded_col][0])).equals(encoded_df[encoded_col])
